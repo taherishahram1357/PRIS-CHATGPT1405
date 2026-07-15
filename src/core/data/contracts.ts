5dc958ca2afc7373b1c606578769de760d886790
taherishahram1357 <taheri.shahram.1357@gmail.com>
2026-07-15 17:32:59 +0330

    feat(data): add contract mock data

---
+++ src/core/data/contracts.ts
@@ -0,0 +1,11 @@
+import type { Contract } from '../domain/contract';
+
+export const contracts: Contract[] = [
+  {
+    id: 'C1',
+    supplierId: 'S1',
+    projectId: 'P1',
+    value: 45000000,
+    deliveryDate: '2026-12-01',
+  },
+];
